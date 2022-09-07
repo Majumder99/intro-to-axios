c3ada63961c5555d32518de1044315e9d95ec1f3
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-07 23:45:48 +0600

    using promises axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+//https://jsonplaceholder.typicode.com/posts
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  //Using promises
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Axios Tutorial</h1>
+      <div className="error">{error ? error : null}</div>
+      {/* {error !== "" && <h2>{error}</h2>} */}
+      <div className="grid">
+        {posts.map((post) => {
+          const { id, title, body } = post;
+          return (
+            <div key={id} className="card">
+              <h1>{title.slice(0, 15).toUpperCase()}</h1>
+              <p>{body.slice(0, 100)}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
